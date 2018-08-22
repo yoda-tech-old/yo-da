ee0902150394d29f47d6d9b8b6d740cad217a67a
Edoardo Paluan <esp@yo-da.co>
2018-08-22 15:34:09 +0100

    Add New File

---
+++ middleware/getPassportHelpers.js
@@ -0,0 +1,43 @@
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth2').Strategy;
+
+passport.serializeUser(function (user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function (obj, done) {
+    done(null, obj);
+});
+
+const googleStrategy = new GoogleStrategy({
+        clientID: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        callbackURL: process.env.CALL_BACK_URL,
+        passReqToCallback: true
+    },
+    function (request, accessToken, refreshToken, profile, done) {
+        process.nextTick(function () {
+            return done(null, profile);
+        });
+    }
+)
+
+passport.use(googleStrategy);
+
+const authenticator = passport.authenticate('google', {
+    scope: [
+        'https://www.googleapis.com/auth/plus.login',
+        'https://www.googleapis.com/auth/plus.profile.emails.read'
+    ]
+})
+
+const callback = passport.authenticate('google', {
+    successRedirect: '/',
+    failureRedirect: '/'
+})
+
+module.exports = {
+    passport,
+    authenticator,
+    callback
+}
